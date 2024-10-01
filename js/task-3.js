4d8e25cba99190e3a7c73cf38b23994688948e4e
dmytro_dykyi <dimadykyiolex02@gmail.com>
2024-10-02 01:20:49 +0300

    d3

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,7 @@ function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding) * 2;
   const borderWidth = parseFloat(border) * 2;
-  const totalWidth = contentWidth + paddingWidth + borderWidth;
-  return totalWidth;
+  return contentWidth + paddingWidth + borderWidth;
 }
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
